1552b02f755b8783efcbdf5a0f3026fc5bef77ef
Miguel Angelo Maiopue Guilovica <miguelangelomg2004@gmail.com>
2026-06-27 14:55:34 +0200

    Update user_repo.py
    Added update sql methods

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -26,32 +26,33 @@ def exe_cursor(sql, values = None, commit = True):
 
 
 class Sql(): 
+    #Insert methods
     @staticmethod
     def set_expire():   # all tokens will expire in 1 minute
             exp = datetime.now() + timedelta(seconds=60)
             return exp
     
     @staticmethod
-    def add_client(username, email, phone_country_code, phone_number, city,
-                    street, postal_code, password_hash, passkey_public_key,
-                    passcode_hash, mfa_secret):
+    def insert_client(username, email, phone_country_code, phone_number, city,
+                    street, postal_code, password_hash, passkey_public_key, 
+                    digital_signature_public_key, passcode_hash, mfa_secret):
                      
 
                     sql = """INSERT INTO Clients (username, email, phone_country_code, phone_number, city,
-                    street, postal_code, password_hash, passkey_public_key,
+                    street, postal_code, password_hash, passkey_public_key, digital_signature_public_key
                     passcode_hash, mfa_secret)
 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                     """
 
                     values = (username, email, phone_country_code, phone_number, city,
                     street, postal_code, password_hash, passkey_public_key,
-                    passcode_hash, mfa_secret)
+                    digital_signature_public_key, passcode_hash, mfa_secret)
 
                     exe_cursor(sql, values)
                     
     @staticmethod
-    def add_clientId(client_id, given_names, last_name, id_type, birthday, 
+    def insert_clientId(client_id, given_names, last_name, id_type, birthday, 
                      nationality, emission_country, issue_date, expiration_date,
                      is_verified
                      ):
@@ -70,7 +71,7 @@ class Sql():
                     exe_cursor(sql, values)
 
     @staticmethod
-    def add_backupCode(client_id, code_hash):
+    def insert_backupCode(client_id, code_hash):
 
                     sql = """INSERT INTO MFA_BackupCodes (client_id, code_hash)
 
@@ -82,7 +83,7 @@ class Sql():
                     exe_cursor(sql, values)
                 
     @staticmethod
-    def add_authHistory(client_id, auth_type, authMethod_hash):
+    def insert_authHistory(client_id, auth_type, authMethod_hash):
 
                     sql = """INSERT INTO AuthMethodHistory (client_id, auth_type, authMethod_hash)
 
@@ -94,7 +95,7 @@ class Sql():
                     exe_cursor(sql, values)
             
     @staticmethod
-    def add_passwordResetToken(client_id, token_hash):
+    def insert_passwordResetToken(client_id, token_hash):
 
                     sql = """INSERT INTO PasswordResetTokens (client_id, expires_at, token_hash)
                           VALUES (%s, %s, %s)
@@ -105,7 +106,7 @@ class Sql():
                     exe_cursor(sql, values)
 
     @staticmethod
-    def add_loginAttempts(client_id, device_fingerprint, user_agent, ip_address,
+    def insert_loginAttempts(client_id, device_fingerprint, user_agent, ip_address,
                                 location, outcome, risk_score):
 
                     sql = """INSERT INTO LoginAttempts (client_id, device_fingerprint, user_agent, ip_address,
@@ -119,7 +120,7 @@ class Sql():
                     exe_cursor(sql, values)
 
     @staticmethod
-    def add_notification(client_id, notification_type, notification_purpose, status):
+    def insert_notification(client_id, notification_type, notification_purpose, status):
             sql = """ INSERT INTO NotificationLog (client_id, notification_type, notification_purpose, status)
 
                       VALUES (%s, %s, %s, %s)
@@ -129,7 +130,7 @@ class Sql():
             exe_cursor(sql, values)
 
     @staticmethod
-    def add_passcodeAttempt(client_id, account_id, outcome, ip_address, location, risk_score):
+    def insert_passcodeAttempt(client_id, account_id, outcome, ip_address, location, risk_score):
             sql = """
                    INSERT INTO PasscodeAttempts (client_id, account_id, outcome, ip_address, location, risk_score)
                    VALUES (%s, %s, %s, %s, %s, %s)
@@ -137,7 +138,183 @@ class Sql():
             values = (client_id, account_id, outcome, ip_address, location, risk_score)
 
             exe_cursor(sql, values)
+######################################################################################################################################################
+    
+    # Update methods
+    @staticmethod
+    def mfa_on(client_id):
+            sql = """
+                    UPDATE Clients
+                    SET mfa_active = %s
+                    WHERE client_id = %s
+                   """
+            values = (True, client_id)
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def mfa_off(client_id):
+            sql = """
+                    UPDATE Clients
+                    SET mfa_active = %s
+                    WHERE client_id = %s
+                   """
+            values = (False, client_id)
+
+            exe_cursor (sql, values)
+
+    @staticmethod
+    def lock_client(client_id):
+            sql = """
+                    UPDATE Clients
+                    SET is_locked = %s
+                    WHERE client_id = %s
+                   """
+            values = (True, client_id) 
+
+            exe_cursor (sql, values)
+    @staticmethod
+    def unlock_client(client_id):
+            sql = """
+                    UPDATE Clients
+                    SET is_locked = %s
+                    WHERE client_id = %s
+                   """
+            values = (False, client_id) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def deactivate_client(client_id):
+            sql = """
+                    UPDATE Clients
+                    SET is_active = %s
+                    WHERE client_id = %s
+                   """
+            values = (False, client_id)
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def activate_client(client_id):
+            sql = """
+                    UPDATE Clients
+                    SET is_active = %s
+                    WHERE client_id = %s
+                   """
+            values = (True, client_id) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def verify_id(client_id):
+            sql = """
+                    UPDATE Clients_ids
+                    SET is_verified = %s
+                    WHERE client_id = %s
+                   """
+            values = (True, client_id)
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def expire_id():
+            sql = """
+                    UPDATE Clients_ids
+                    SET is_verified = %s
+                    WHERE expiration_date < NOW ()
+                   """
+            values = (False,) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def change_pwd(new_pwd, client_id):
+            sql = """
+                    UPDATE Clients
+                    SET password_hash = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (new_pwd, client_id) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def change_passcode(new_passcode, client_id):
+            sql = """
+                    UPDATE Clients
+                    SET passcode_hash = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (new_passcode, client_id) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def change_secret(new_secret, client_id):
+            sql = """
+                    UPDATE Clients
+                    SET mfa_secret = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (new_secret, client_id) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def change_fidoKey(new_key, client_id):
+            sql = """
+                    UPDATE Clients
+                    SET passkey_public_key = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (new_key, client_id) 
+
+            exe_cursor (sql, values)
+    
+    @staticmethod
+    def change_signKey(new_key, client_id):
+            sql = """
+                    UPDATE Clients
+                    SET digital_signature_public_key = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (new_key, client_id) 
+
+            exe_cursor (sql, values)
+
+    @staticmethod
+    def backup_used(client_id):
+            sql = """
+                    UPDATE MFA_BackupCodes
+                    SET used = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (True, client_id) 
+
+            exe_cursor (sql, values)
+
+    @staticmethod
+    def token_used(client_id):
+            sql = """
+                    UPDATE PasswordResetTokens
+                    SET used = %s
+                    WHERE client_id = %s
+                   """
+                
+            values = (True, client_id) 
+
+            exe_cursor (sql, values)
+    
             
+    
+   
     
             
         
