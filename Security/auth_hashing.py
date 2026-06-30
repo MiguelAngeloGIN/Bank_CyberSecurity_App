ea66d4eaf82345fd1aa73788cb29f03dbc4e8142
Miguel Angelo Maiopue Guilovica <miguelangelomg2004@gmail.com>
2026-06-30 23:55:26 +0200

    Update on auth_hashing.py
    Added hashing for mfa bacup codes too

--- Security/auth_hashing.py
+++ Security/auth_hashing.py
@@ -16,7 +16,26 @@ class PwdHasher:
   except VerifyMismatchError:
    return False
   
+class MfaBackupHasher:
+ mh = PasswordHasher()
+
+ @staticmethod
+ def hash_mfaCode(code):
+  hashed_password = MfaBackupHasher.mh.hash(code)
+  return hashed_password
+ 
+ @staticmethod
+ def verify_password(stored_hash, new_code):
+  try: 
+   MfaBackupHasher.mh.verify(stored_hash, new_code)
+   return True
+  except VerifyMismatchError:
+   return False
   
+  
+  
+
+
   
 
 
